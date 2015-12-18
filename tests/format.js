a9b78860f93f33782bb7cb50ede2fac6b12acc76
Blazh <iblazevi@gmail.com>
2015-12-18 16:34:39 +0100

    Add rest of unit tests

--- tests/format.js
+++ tests/format.js
@@ -3,7 +3,22 @@ var textural = require('../textural');
 exports.format = {
     default: function (test) {
 
-        test.strictEqual(textural('testText').format('snake'), 'test_text');
+        var tests = [
+            ['camel', 'exampleText'],
+            ['snake', 'example_text'],
+            ['slug', 'example-text'],
+            ['human', 'example text'],
+            ['t3', 'exa'],
+            ['uppersnake', 'EXAMPLE_TEXT'],
+            ['lowersnake', 'example_text'],
+            ['capitalizesnake', 'Example_text']
+        ]
+
+        test.expect(tests.length);
+
+        for (i = 0; i < tests.length; i++) {
+            test.strictEqual(textural('exampleText').format(tests[i][0]), tests[i][1]);
+        }
 
         test.done();
     }
