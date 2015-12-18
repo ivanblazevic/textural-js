d485f969735b7367926a9621fc40419bfc6224bd
Blazh <iblazevi@gmail.com>
2015-12-18 17:11:03 +0100

    Drop unused line

--- textural.js
+++ textural.js
@@ -1,6 +1,6 @@
 /*!
  * textural.js
- * version : 0.1.4
+ * version : 0.1.5
  * author : Ivan Blazevic
  * license : MIT
  * git: https://github.com/ivanblazevic/textural-js
@@ -14,7 +14,7 @@
     ************************************/
 
     var textural,
-        VERSION = '0.1.4',
+        VERSION = '0.1.5',
         // check for nodeJS
         hasModule = (typeof module !== 'undefined' && module.exports);
 
@@ -93,7 +93,6 @@
             return parsed;
         }
 
-        return parsed;       
     }
 
     function transform (input, format) {
